8b8792dcdf757eb8e2e7464b3b02fc47a4cd15a7
Сагындык <xaxbcczyzaxab@gmail.com>
2022-05-09 19:00:35 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double[] array = {1.1,-7.2,-1.3,1.4,1.5,-2.2,2.5,-6.3,-5.4,5.2,6.8,2.1,8.4,4.4,-5.5};
+
+        int counterPositiveNum = 0;
+        int counterNegativeNum = 0;
+
+        double sum1 = 0;
+        double sum2 = 0;
+
+        boolean leverArm = false;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0){
+                leverArm = true;
+            }else if (leverArm == true && array[i] > 0){
+                sum1 = sum1 + array[i];
+                counterPositiveNum++;
+            }
+            if (array[i] > 0){
+                leverArm = true;
+            }else if (leverArm == true && array[i] < 0){
+                sum2 = sum2 + array[i];
+                counterNegativeNum++;
+            }
+        }
+        System.out.println("Среднее значение положительных чисел: "+sum1/counterPositiveNum);
+        System.out.println("Среднее значение отрицательных чисел: "+sum2/counterNegativeNum);
+        double sum3 = sum1/counterPositiveNum + sum2/counterNegativeNum;
+        System.out.println("Разница положительных и отрицательных чисел: "+ sum3);
+        arraySort(array);
+    }
+
+
+
+    public static void arraySort (double[] arrayToSort){
+        int indexMin;
+        for (int index = 0; index < arrayToSort.length-1; index++) {
+            indexMin = index;
+            for (int indexToScan = index; indexToScan < arrayToSort.length; indexToScan++) {
+                if (arrayToSort[indexMin] > arrayToSort[indexToScan]){
+                    indexMin = indexToScan;
+                }
+            }
+            double temp = arrayToSort[index];
+            arrayToSort[index] = arrayToSort[indexMin];
+            arrayToSort[indexMin] = temp;
+        }
+        System.out.println(Arrays.toString(arrayToSort));
+    }
+}
